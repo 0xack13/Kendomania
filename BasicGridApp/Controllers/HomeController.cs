976a7eb4f775074044eda12cb56befe083494540
0xack13 <0xack13@gmail.com>
2014-08-15 08:57:28 +0300

    Add new Person model with MVC web project

---
+++ BasicGridApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using BasicGridApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicGridApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Person p = new Person();
+            List<Person> lp = new List<Person>();
+            
+            p.ID = 1;
+            p.FirstName = "Mohammad";
+            p.LastName = "Ali";
+            lp.Add(p);
+
+            p.ID = 2;
+            p.FirstName = "Mohammad";
+            p.LastName = "Ali";
+            lp.Add(p);
+
+            p.ID = 3;
+            p.FirstName = "Mohammad";
+            p.LastName = "Ali";
+            lp.Add(p);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BasicGridApp/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicGridApp.Models
+{
+    public class Person
+    {
+        public Int32 ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
